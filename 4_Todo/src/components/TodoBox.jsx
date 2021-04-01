baf1105742b1aa5087e5995bc22f05b179ead884
Hardtack <rjsdn827@gmail.com>
2021-04-01 15:58:39 +0900

    temp: 일단 적용은 백업

--- 4_Todo/src/components/TodoBox.jsx
+++ 4_Todo/src/components/TodoBox.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
+
 
 const BOX = styled.div`
     border: 1px solid grey;
 
     margin: 5px;
+
+    
+
+    /* & button{
+        width: 80px;
+    }  */
 `
+
+const Remove = () =>{
+   
+}
 
 
 const TodoBox = (props) => { //넘어오는 props 파라미터 이름은 맘대로 정할 수 있지만 보기 쉬운 이름으로 설정, {} 써서 받는 거면 비구조화를 해서 받는거여서 그냥 객체를 넘겨와도 됨
-    return (
+    return (                    //넘어오는 순서는 상관없음
         <div>
             <BOX>
                 <h3>{props.TITLE}</h3>
                 <p>{props.CONTENT}</p>
+
+                <button onClick={Remove} >X</button>
             </BOX>
+
+            
         </div>
     )
 }
--- 4_Todo/src/components/todo.jsx
+++ 4_Todo/src/components/todo.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react' //{} 가져올떄 비구조화 아니면 React.어쩌구로 불러와야 됨
 import TodoBox from './TodoBox'
 
 const Todo = () => {
 
-    const [input, setInput] = useState({
+    
+
+    const [input, setInput] = useState({ 
+        id: '',
         title: '',
         content: '',
     });
 
-    const [list, setList] = useState([
+    const [list, setList] = useState([  //list배열의 newArray를 map으로 생성, 새로 생성된 newArray는 TodoBox 어러개를 담게 됨, 그거를 컴포넌트 프롭스로 보내서 styled 컴포넌트로 렌더링 이라고 이해함
         //array's elements(객채들) here
     ]);
 
@@ -20,22 +23,33 @@ const Todo = () => {
         })
     }
 
+    const nextId = useRef(1); //useRef사용 - 타이머 만들때도 사용
+
     const onClick = () => { //배열 안에 객체가 들어있으므로
         setList([ //배열 안에 객체 리ㅏ넘어ㅑㅣㅏㅇㄹㄴㅁ커;ㅣㅏㅁㄹㄴ아ㅣ
             ...list,
+        
             {
-                title: input.title,
-                content: input.content,
+                id: nextId.current, //useRef는 객체인데, current를 사용해주서 그냥 변수 취급 :D
+
+                title: input.title,       //input useState 객체의 title
+                content: input.content,    //input useState 객체의 content
                    
             },
         ]);
 
+        console.log(nextId.current)
+        nextId.current += 1; //useRef는 객체인데, current를 사용해주서 그냥 변수 취급 :D
+         
+
         // const newArray = list.map()
     }
 
-    const newArray = list.map(x => <TodoBox TITLE={x.title} CONTENT={x.content}/>) //x는 객체여서 위에 input 객체 에서 title과 content를 가져올 수 있음       <div>{x.title}{x.content} </div>
+    const newArray = list.map(x => <TodoBox SETLIST={setList} LIST={list} ID={x.id} TITLE={x.title} CONTENT={x.content}/>) //x는 객체여서 위에 input 객체 에서 title과 content를 가져올 수 있음, TodoBox가 담긴 x를 복사해서 return       <div>{x.title}{x.content} </div>
 
-    //const nextId = useRef(0);
+
+    
+
 
     // const array = [<div>a</div>, <div>b</div>];
     return (

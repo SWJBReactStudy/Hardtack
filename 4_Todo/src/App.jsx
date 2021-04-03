534b0f610ba6d57b4a11d1f7b2cc8077388ccd6f
HARDTACK-Dev <rjsdn827@gmail.com>
2021-04-03 17:05:50 +0900

    chore: 어떤 컴포넌트를 보일지 update

--- 4_Todo/src/App.jsx
+++ 4_Todo/src/App.jsx
@@ -1,11 +1,12 @@
-import React, {useState} from 'react'
-import Todo from './components/todo.jsx'
+import React from 'react'
+//import Todo from './components/todo.jsx'
+import NewTodo from './components/Refactored/NewTodo.jsx' //import 이름은 export 한 것으로 해줘야 됨!
 
 const App = () =>{
 
   return(
     <div>
-      <Todo/>
+      <NewTodo/>
     </div>
   )
 }

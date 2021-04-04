f3bfe263710566f6bb621701fcf8f055a905cb09
HARDTACK-Dev <rjsdn827@gmail.com>
2021-04-04 18:41:08 +0900

    chore: 자잘한 수정

---
+++ 5_Hooks/src/App.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import Todo from './components/todo.jsx'
+import Effect from './components/useEffect.jsx'
+
+const App = () =>{
+
+  return(
+    <div>
+     <Effect/>
+      <Todo/>
+    </div>
+  )
+}
+export default App;
